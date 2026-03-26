d958763245fb739af4f63b148fdbc743b2b4c932
sxndzjxtt-hub <sxndzjxtt@gmail.com>
2026-03-26 21:46:49 +0530

    Add 3D tilt effect to glass card on mouse move

--- script.js
+++ script.js
@@ -51,3 +51,11 @@ function openLogin() {
 function closeLogin() {
   document.getElementById("loginModal").style.display = "none";
 }
+const card = document.querySelector(".glass-card");
+
+document.addEventListener("mousemove", (e) => {
+  const x = (window.innerWidth / 2 - e.clientX) / 25;
+  const y = (window.innerHeight / 2 - e.clientY) / 25;
+
+  card.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
+});
